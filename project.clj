6544d0cde29998dd545a2656ca6a39169a7e371b
Rahul Goma Phulore <rahul.phulore.999@gmail.com>
2018-04-08 19:23:47 +0100

    Downgrade Clojure to 1.8.0
    Because Gorilla REPL isn't available for 1.9.0.

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
                  "A Clojure implementation of a tree-walking interpreter for Lox language"
                  "from the excellent book 'Crafting Interpreters'")
   :url "https://github.com/missingfaktor/la-lingua-loxa"
-  :dependencies [[org.clojure/clojure "1.9.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
                  [akar/akar-core "1.0.0"]
                  [org.blancas/kern "1.1.0"]
                  [com.bhauman/rebel-readline "0.1.2"]]
